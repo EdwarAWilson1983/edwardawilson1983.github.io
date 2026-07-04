34b0daf2bdeafc2638d8e9d1fe43756c4b91e2ac
EdwarAWilson1983 <edwardawilson1983@outlook.com>
2026-07-03 23:58:03 -0400

    Update america250.js

--- higher-ideals/america-250/america250.js
+++ higher-ideals/america-250/america250.js
@@ -193,3 +193,37 @@ if(article && readingTime){
         `${minutes} minute${minutes > 1 ? "s" : ""} read`;
 
 }
+
+/* ===========================================
+   SMOOTH TOC
+=========================================== */
+
+document
+.querySelectorAll(".toc a")
+.forEach(link=>{
+
+    link.addEventListener("click",e=>{
+
+        e.preventDefault();
+
+        const target=document.querySelector(
+
+            link.getAttribute("href")
+
+        );
+
+        if(target){
+
+            target.scrollIntoView({
+
+                behavior:"smooth",
+
+                block:"start"
+
+            });
+
+        }
+
+    });
+
+});
